3f14c09a492090b9b0ebc9ab3983c7b87e89909c
Mahi-0420 <2410056@i-seifu.jp>
2025-04-30 10:32:22 +0900

    新規ファイル追加
    ここは詳細な説明を書くとよいです。

---
+++ Main2.java
@@ -0,0 +1,23 @@
+/*
+C言語で書くと
+// #include<stdio.h>
+
+ int main() {
+     printf("Hello, World!\n");
+    　return 0;
+ }
+*/
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("こんにちは");
+    }
+}
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println(3 +1);
+        int age = 20;
+        double height = 1.75;  }
+}
